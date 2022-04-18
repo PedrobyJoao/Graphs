a60c6c26be4ca2d692023252b0502ac312a96e58
Joao <joaobk09@gmail.com>
2022-04-17 21:02:25 -0300

    Adding cost hash table

--- dijkstra_algo.py
+++ dijkstra_algo.py
@@ -32,15 +32,17 @@ graph['end'] = {}
 
 '''
 
- * Now we have all information about nodes, their neighbors and the weights of each edge between two vertices
- * We have now to create the PARENTS-TABLE so that we know which is the lowest weight edge
- * Example, TO the node 'A' there are two paths: 'B' and 'Start', we will have to update the parents of A accordinly to the weight,
- always looking for the lowest one.
+ * Now we have all information about the nodes, their neighbors and the weights of each edge between two vertices
+ * We have now to create the PARENTS-TABLE so that we know which is the shortest path through the parents, which are based on the lowest weight
+ * We also have to create a COST-TABLE to keep track and make updates of the lowest weight-cost to get to a node
 
 '''
 
 # Parents hash table (The key is the son and the value the parent)
 parents = {}
+
+# Cost hash table (Key == node, Value == cost)
+cost = {}
 
 # array of updated nodes
 updated_nodes = []
@@ -53,9 +55,14 @@ def lowest_weight(node):
         if graph[node][key] < lowest_weight:
             lowest_weight = graph[node][key]
             lowest_node = key
-    return lowest_node
+    return lowest_node, lowest_weight
 
 # Dijkstra algorithm to find the shortest path
+def dijkstra():
+    next_node = lowest_weight("start")[0]
+    for path in graph[next_node]:
+        graph[next_node][path] = 
+    print("Teste")
 
-lowest_weight('start')
-#print(graph[0])
+for i in graph['b']:
+    print(i)
